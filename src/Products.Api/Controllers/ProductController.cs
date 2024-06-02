5ab2a5dfcd63060aa7f459c2137287dea388ab3a
ingsisrags@hotmail.com <ingsisrags@hotmail.com>
2024-06-02 08:41:22 -0500

    add some tags for swagger

--- src/Products.Api/Controllers/ProductController.cs
+++ src/Products.Api/Controllers/ProductController.cs
@@ -4,6 +4,7 @@ using Application.Queries;
 using Domain.Common;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
 using System.Net;
 
 namespace Products.Api.Controllers
@@ -21,14 +22,24 @@ namespace Products.Api.Controllers
             _mediator = mediator;
         }
 
-        /// <summary>
-        /// Method find product by Id
-        /// </summary>
-        /// <param name="Id"></param>
-        /// <returns name="product"> </returns>
 
-        [HttpGet("v1/get-product-by-id/{Id}")]
-        [ProducesResponseType(typeof(Response<ProductDto>), (int)HttpStatusCode.OK)]
+        /// <summary>
+        /// Finds a product by its ID.
+        /// </summary>
+        /// <param name="id">The ID of the product to find.</param>
+        /// <returns>The product with the specified ID.</returns>
+        /// <response code="200">Returns the product with the specified ID.</response>
+        /// <response code="404">If the product is not found.</response>
+        /// <response code="500">If exists internal error. </response>
+        /// 
+        [HttpGet("v1/get-product-by-id/{id}")]
+        [ProducesResponseType(typeof(ProductDto), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        [SwaggerOperation(
+            Summary = "Get product by ID",
+            Description = "Retrieves a product byID .",
+            OperationId = "GetProductById"
+        )]
         public async Task<ProductDto> GetPropertiesById([FromRoute] int Id)
         {
             var product = await _mediator.Send(new GetProductByIdQuery() { Id = Id });
@@ -37,13 +48,22 @@ namespace Products.Api.Controllers
 
 
         /// <summary>
-        /// Method for create product 
+        /// Creates a new product.
         /// </summary>
-        /// <param name="command"></param>
-        /// <returns name="product"></returns>
-
+        /// <param name="command">For execute create product command.</param>
+        /// <returns>The created product.</returns>
+        /// <response code="200">Returns the created product.</response>
+        /// <response code="400">If the product data is invalid.</response>
+        /// <response code="500">If exist internal error.</response>
         [HttpPost("v1/create-product")]
-        [ProducesResponseType(typeof(Response<ProductDto>), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(ProductDto), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        [SwaggerOperation(
+            Summary = "Create a new product",
+            Description = "Creates a new product",
+            OperationId = "CreateProduct",
+            Tags = new[] { "Product" }
+        )]
         public async Task<ProductDto> CreateProduct([FromForm] CreateProductCommand command)
         {
             var product = await _mediator.Send(command);
@@ -53,14 +73,24 @@ namespace Products.Api.Controllers
         }
 
         /// <summary>
-        /// Method for update product
+        /// Updates an existing product.
         /// </summary>
-        /// <param name="command"></param>
-        /// <returns name="product"></returns>
-
-
+        /// <param name="command">The command is the body for execute command.</param>
+        /// <returns>The updated product.</returns>
+        /// <response code="200">Returns the updated product.</response>
+        /// <response code="400">If the product data is invalid.</response>
+        /// <response code="404">If the product is not found.</response>
+        /// <response code="500">If exist internal error.</response>
         [HttpPut("v1/update-product")]
-        [ProducesResponseType(typeof(Response<ProductDto>), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(ProductDto), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        [SwaggerOperation(
+            Summary = "Update an existing product",
+            Description = "Updates an existing product in the database.",
+            OperationId = "UpdateProduct",
+            Tags = new[] { "Product" }
+        )]
         public async Task<ProductDto> UpdateProduct([FromForm] UpdateProductCommand command)
         {
             var product = await _mediator.Send(command);
